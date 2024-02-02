2ae544d6db99317b1b6e1bf3310805313cec822c
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-02 07:59:04 +0300

    add jumia icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,25 @@
-import styles from './Navbar.module.css';
-// import NavbarGif from '../../assets/nav/navbar.gif'
-// import freeDelivery from '../../assets/nav/freeDelivery.gif';
-import freeDelivery1 from '../../assets/nav/freedelivery1.gif';
+import styles from "./Navbar.module.css";
+import freeDelivery1 from "../../assets/nav/freedelivery1.gif";
+import jumiakenya from "../../assets/nav/jumia.png";
 
 function Navbar() {
   return (
     <div className={styles.container}>
-        <div className={styles.navbarImage}>
-          <img src={freeDelivery1} alt='navbar-gif'/>
+      <div className={styles.navbarImage}>
+        <img src={freeDelivery1} alt="navbar-gif" />
+      </div>
+      <div className={styles.iconsContainer}>
+        <div className={styles.firstIcon}>
+          <a href="/">Sell on Jumia</a>
         </div>
-      
+        <div className={styles.search}>
+          <div className={styles.innerContainer}>
+            <img src={jumiakenya} alt="jumia-kenya" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
