f7a2b960781e9fdd845748678dd580ff7c8d8296
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-11 12:04:12 +0300

    add the icon section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -22,18 +22,18 @@ function Home() {
     <section className={styles.container}>
         <div className={styles.topBar}>
             <div className={styles.menu}>
-                <a href='/officailStore'><FontAwesomeIcon icon={faBagShopping} />Official Stores</a>
-                <a href='/phones&tablets'><FontAwesomeIcon icon={faMobile} />Phones & Tablets</a>
-                <a href='/tv&audio'><FontAwesomeIcon icon={faRadio} /></a>
-                <a href='appliances'><FontAwesomeIcon icon={faGasPump} />Appliances</a>
-                <a href='/Health&Beauty'><FontAwesomeIcon icon={faBottleDroplet} />Health & Beauty</a>
-                <a href='home&office'><FontAwesomeIcon icon={faHotel} />Home & Office</a>
-                <a href='/fashion'><FontAwesomeIcon icon={faShirt} />Fashion</a>
-                <a href='/computing'><FontAwesomeIcon icon={faDesktop} />Computing</a>
-                <a href='/supermarket'><FontAwesomeIcon icon={faAppleWhole} />Supermarket</a>
-                <a href='/babyproducts'><FontAwesomeIcon icon={faFaceGrinBeamSweat} />Baby Products</a>
-                <a href='/Sporting Goods'><FontAwesomeIcon icon={faDumbbell} />Sporting Goods</a>
-                <a href='/othergoods'><FontAwesomeIcon icon={faEllipsis} />Other categories</a>
+                <a href='/officailStore'><FontAwesomeIcon icon={faBagShopping} className={styles.fontAwesomeIcon} />Official Stores</a>
+                <a href='/phones&tablets'><FontAwesomeIcon icon={faMobile} className={styles.fontAwesomeIcon} />Phones & Tablets</a>
+                <a href='/tv&audio'><FontAwesomeIcon icon={faRadio} className={styles.fontAwesomeIcon} />TVs & Audio</a>
+                <a href='appliances'><FontAwesomeIcon icon={faGasPump} className={styles.fontAwesomeIcon} />Appliances</a>
+                <a href='/Health&Beauty'><FontAwesomeIcon icon={faBottleDroplet} className={styles.fontAwesomeIcon} />Health & Beauty</a>
+                <a href='home&office'><FontAwesomeIcon icon={faHotel} className={styles.fontAwesomeIcon} />Home & Office</a>
+                <a href='/fashion'><FontAwesomeIcon icon={faShirt} className={styles.fontAwesomeIcon} />Fashion</a>
+                <a href='/computing'><FontAwesomeIcon icon={faDesktop} className={styles.fontAwesomeIcon} />Computing</a>
+                <a href='/supermarket'><FontAwesomeIcon icon={faAppleWhole} className={styles.fontAwesomeIcon}/>Supermarket</a>
+                <a href='/babyproducts'><FontAwesomeIcon icon={faFaceGrinBeamSweat}className={styles.fontAwesomeIcon} />Baby Products</a>
+                <a href='/Sporting Goods'><FontAwesomeIcon icon={faDumbbell}className={styles.fontAwesomeIcon} />Sporting Goods</a>
+                <a href='/othergoods'><FontAwesomeIcon icon={faEllipsis}className={styles.fontAwesomeIcon} />Other categories</a>
 
             </div>
 
