dd1e3d684bf95852fb33832b9bd3718dffefd3b1
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-12 09:42:06 +0300

    finalize the helpCentre div

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -25,7 +25,7 @@ import {
   faEllipsis,
   faCircleQuestion,
   faArrowsRotate,
-  faMoneyBill1Wave
+  faMoneyBill1Wave,
 } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
@@ -151,27 +151,34 @@ function Home() {
       <div className={styles.helpCentre}>
         <div className={styles.TopHelpCentre}>
           <div className={styles.items}>
-            <FontAwesomeIcon icon={faCircleQuestion} className={styles.itemsListIcons} />
-            <div className={styles.itemsList}> 
-            <a href="/helpcentre">HELP CENTER</a>
-            <p>Guide To Customer Care</p>
+            <FontAwesomeIcon
+              icon={faCircleQuestion}
+              className={styles.itemsListIcons}
+            />
+            <div className={styles.itemsList}>
+              <a href="/helpcentre">HELP CENTER</a>
+              <p>Guide To Customer Care</p>
             </div>
           </div>
           <div className={styles.items}>
-          <FontAwesomeIcon icon={faArrowsRotate}  className={styles.itemsListIcons} />
-          <div className={styles.itemsList}> 
-            <a href="/easyreturn">EASY RETURN</a>
-            <p>Quick refund</p>
+            <FontAwesomeIcon
+              icon={faArrowsRotate}
+              className={styles.itemsListIcons}
+            />
+            <div className={styles.itemsList}>
+              <a href="/easyreturn">EASY RETURN</a>
+              <p>Quick refund</p>
             </div>
           </div>
           <div className={styles.items}>
-          <FontAwesomeIcon icon={faMoneyBill1Wave}  className={styles.itemsListIcons} />
-          <div className={styles.itemsList}>
-            <a href="/sellOnJumia">SELL ON JUMIA</a>
-            <p>Millions of Visitors</p>
-
-          </div>
-
+            <FontAwesomeIcon
+              icon={faMoneyBill1Wave}
+              className={styles.itemsListIcons}
+            />
+            <div className={styles.itemsList}>
+              <a href="/sellOnJumia">SELL ON JUMIA</a>
+              <p>Millions of Visitors</p>
+            </div>
           </div>
         </div>
         <div className={styles.bottomHelpCentre}>
