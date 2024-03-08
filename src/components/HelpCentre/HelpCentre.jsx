d69e371b7aa4e403b4df315783cb6a2572c7629e
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-08 23:31:44 +0300

    add order divs

--- src/components/HelpCentre/HelpCentre.jsx
+++ src/components/HelpCentre/HelpCentre.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import styles from './HelpCentre.module.css';
+import React from "react";
+import styles from "./HelpCentre.module.css";
+import OrdersData from "../../Data/helpcentre.json";
 
 function HelpCentre() {
   return (
     <section className={styles.container}>
-        <div className={styles.helpCentreContainer}>
-            <div className={styles.help}>
-                <h3>Help Center</h3>
-                <h3> Hi, how can we help you?</h3>
-            </div>
-
+      <div className={styles.helpCentreContainer}>
+        
+        <div className={styles.helpCentre}>
+          <h4>Help Center</h4>
+          <h3> Hi, how can we help you?</h3>
         </div>
 
+        <div className={styles.help}>
+          {OrdersData.map((order, id) => (
+            <div key={id} className={styles.orders}>
+              <h3>{order.title}</h3>
+              <img src={order.helpCentreImage} />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default HelpCentre
+export default HelpCentre;
